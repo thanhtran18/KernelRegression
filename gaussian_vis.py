8031e50c2c367260feb9383a352e15dc78c9876c
thanhtran18 <tranct@myumanitoba.ca>
2019-03-22 16:13:03 -0500

    Completed the guassian 1d visualization

--- gaussian_vis.py
+++ gaussian_vis.py
@@ -4,17 +4,15 @@ import matplotlib.pyplot as plt
 import math as math
 
 
-def evaluateGaussianKernel(X_test, t_test, X_train, t_train, h):
+def evaluate_gaussian_kernel(X_test, t_test, X_train, t_train, h):
     G = (1 / math.sqrt(2 * math.pi * h**2)) * np.exp(-utils.dist2(X_test.reshape(X_test.shape[0], 1), X_train) / (2 * h**2))
     T = np.tile(t_train.T, [X_test.shape[0], 1])
     denom = np.sum(G, 1)
     default_value = np.mean(t_train)
 
     t_est = np.sum(G * T, 1) / (denom + np.spacing(1))
-    # np.where(t)
-    b = np.where(denom < np.spacing(1))
-    # t_est[np.nonzero(denom < np.spacing(1))] = default_value
-    t_est[b] = default_value
+    chosen_indices = np.where(denom < np.spacing(1))
+    t_est[chosen_indices] = default_value
     if len(t_test) > 0:
         test_err = np.sqrt(np.mean((t_test - t_est) ** 2))
     else:
@@ -26,7 +24,7 @@ def evaluateGaussianKernel(X_test, t_test, X_train, t_train, h):
 X_n = utils.normalizeData(X)
 t = utils.normalizeData(t)
 
-X_n = X_n[:, 4].reshape(X_n.shape[0], 1)
+X_n = X_n[:, 2].reshape(X_n.shape[0], 1)
 
 # CREATE THE TRAIN AND TEST SETS:
 # ================================
@@ -39,19 +37,17 @@ t_test = t_test.reshape(t_test.shape[0], 1)
 t_train = t_train.reshape(t_train.shape[0], 1)
 
 
-
-hs = [0.001, 0.01, 0.1, 0.25, 1, 2, 3, 4]
+h_values = [0.01, 0.1, 0.25, 1, 2, 3, 4]
 # hs = [1]
 
-for i in range(len(hs)):
-    x_ev = np.arange(min(X_train), max(X_train), 1)
-    # y_ev = evaluateGaussianKernel(x_ev, [], X_train, t_train, hs[i])
-    [y_ev, test_err] = evaluateGaussianKernel(x_ev, t_test, X_train, t_train, hs[i])
+for h in h_values:
+    x_ev = np.arange(min(X_train), max(X_train), 0.01)
+    [y_ev, test_err] = evaluate_gaussian_kernel(x_ev, t_test, X_train, t_train, h)
 
     plt.plot(x_ev, y_ev, 'r.-')
     plt.plot(X_train, t_train, 'gx', markersize=10)
     plt.plot(X_test, t_test, 'bo', markersize=10, mfc='none')
     plt.xlabel('x')
     plt.ylabel('t')
-    plt.title('Fit with h=%.3f' % hs[i])
+    plt.title('Fit with h=%.3f' % h)
     plt.show()
